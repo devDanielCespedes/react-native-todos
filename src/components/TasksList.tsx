a3a78a73a60dcbd664077d0c3d4c52cacea5a668
Daniel Cespedes <dev.danielcespedes@gmail.com>
2022-01-09 16:34:40 -0300

    feat: add functionalities of the app
    This commit implements the add todo, remove todo and mark as done todo
    functions and the style for each item in the list.

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -21,7 +21,7 @@ interface TasksListProps {
 export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps) {
   return (
     <FlatList
-      // data={tasks}
+      data={tasks}
       keyExtractor={item => String(item.id)}
       contentContainerStyle={{ paddingBottom: 24 }}
       showsVerticalScrollIndicator={false}
@@ -33,38 +33,28 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 testID={`button-${index}`}
                 activeOpacity={0.7}
                 style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
+                onPress={() => toggleTaskDone(item.id)}
               >
-                <View 
+                <View
                   testID={`marker-${index}`}
-                  //TODO - use style prop 
+                  style={!item.done ? styles.taskMarker : styles.taskMarkerDone}
                 >
-                  { item.done && (
-                    <Icon 
-                      name="check"
-                      size={12}
-                      color="#FFF"
-                    />
-                  )}
+                  {item.done && <Icon name="check" size={12} color="#FFF" />}
                 </View>
 
-                <Text 
-                  //TODO - use style prop
-                >
-                  {item.title}
-                </Text>
+                <Text style={!item.done ? styles.taskText : styles.taskTextDone}>{item.title}</Text>
               </TouchableOpacity>
             </View>
 
             <TouchableOpacity
               testID={`trash-${index}`}
               style={{ paddingHorizontal: 24 }}
-              //TODO - use onPress (remove task) prop
+              onPress={() => removeTask(item.id)}
             >
               <Image source={trashIcon} />
             </TouchableOpacity>
           </ItemWrapper>
-        )
+        );
       }}
       style={{
         marginTop: 32
